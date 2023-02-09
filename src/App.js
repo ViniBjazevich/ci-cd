940b2866a513ec0aecd6f36b5deec0038cdb7bec
Vini Bjazevich <vinibjazevich@MacBook-Pro.local>
2023-02-08 20:46:52 -0600

    feat: added lint to github repo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>CI/CD Example</h1>
       <p>This an example of continuous integration and delivery using github actions.</p>
+      <a href="https://github.com/ViniBjazevich/ci-cd">Github Repo</a>
     </div>
   );
 }
